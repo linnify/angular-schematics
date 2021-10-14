b43435a05812ee33000f888730cacce0c1421e25
Bianca Mateiu <biancamateiu@Biancas-Mac-mini.local>
2021-10-14 19:26:01 +0300

    improve module schematic

--- src/utils/workspace.utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function createHost(tree: any): {
-    readFile(path: any): Promise<any>;
-    writeFile(path: any, data: any): Promise<any>;
-    isDirectory(path: any): Promise<boolean>;
-    isFile(path: any): Promise<any>;
-};
