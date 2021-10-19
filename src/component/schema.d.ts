67063df73c0a6e60a461d092243cd1975ce77752
Bianca Mateiu <biancamateiu@Biancas-Mac-mini.local>
2021-10-19 14:48:49 +0300

    fix component schematic

--- src/component/schema.d.ts
+++ src/component/schema.d.ts
@@ -2,4 +2,5 @@ import {Schema as ComponentSchema} from '@schematics/angular/component/schema';
 
 export interface Schema extends ComponentSchema {
   skipIndexImport: boolean;
+  indexExport: boolean;
 }
